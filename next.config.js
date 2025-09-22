70e4e0c7d569f9e1ad6746eadf7b718b12da1ac6
tylerrleee <160276402+tylerrleee@users.noreply.github.com>
2025-09-22 11:51:03 -0400

    global.css

--- next.config.js
+++ next.config.js
@@ -4,10 +4,7 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/august-portfolio' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/august-portfolio' : '',
-  outputFileTracingRoot: __dirname
+  }
 }
 
 module.exports = nextConfig
--- vercel.sh
+++
@@ -1,5 +0,0 @@
-if [[ $VERCEL_ENV == "production"  ]] ; then
-  npm run build:production
-else
-  npm run build:preview
-fi
