3fb24cb7501cd69e382ed60c52b89689625c1e05
tylerrleee <160276402+tylerrleee@users.noreply.github.com>
2025-09-22 11:46:07 -0400

    global.css

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,35 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:[],rootMainFilesTree:{},pages:{"/":["static/chunks/webpack-81056dc6b90a12a8.js","static/chunks/framework-b1e5f14688f9ffe6.js","static/chunks/main-8c7a9b9ad297eca7.js","static/chunks/967-e5d455d1af52326f.js","static/chunks/pages/index-e6b6eeca3b9a2c54.js"],"/_app":["static/chunks/webpack-81056dc6b90a12a8.js","static/chunks/framework-b1e5f14688f9ffe6.js","static/chunks/main-8c7a9b9ad297eca7.js","static/css/4b3a1a576ec2db8e.css","static/chunks/pages/_app-4734b3ee693b72ef.js"],"/_error":["static/chunks/webpack-81056dc6b90a12a8.js","static/chunks/framework-b1e5f14688f9ffe6.js","static/chunks/main-8c7a9b9ad297eca7.js","static/chunks/pages/_error-5366f00fee9a89ba.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [
+    "static/chunks/react-refresh.js"
+  ],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/index.js"
+    ],
+    "/_app": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_app.js"
+    ],
+    "/_error": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_error.js"
+    ]
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
---
+++ vercel.sh
@@ -0,0 +1,5 @@
+if [[ $VERCEL_ENV == "production"  ]] ; then
+  npm run build:production
+else
+  npm run build:preview
+fi
